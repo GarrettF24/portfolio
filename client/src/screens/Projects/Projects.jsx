bec6024dcdefeb6a1aa6bbbbad8df0c2c1846295
GarrettF <grfoster24@gmail.com>
2021-10-18 12:47:04 -0400

    working on projects, going to try and restyle footer to stick to bottom

--- client/src/screens/Projects/Projects.jsx
+++ client/src/screens/Projects/Projects.jsx
@@ -2,47 +2,98 @@ import React from "react"
 import "./Projects.css"
 import MuayThaiPicture from "../../assets/muay-thai-guy.png"
 import GJAM from "../../assets/gjam.png"
+import FITNESS from "../../assets/fitnessapp.png"
 export default function Projects() {
   return (
     <div className="project-container">
       <div className="projects">
         <div className="project-screenshot">
-          <a className="anchor" href="https://muay-thai-guy.netlify.app/">
+          <a
+            target="_blank"
+            className="anchor"
+            href="https://muay-thai-guy.netlify.app/"
+          >
             <img className="screenshot" src={MuayThaiPicture} alt="project1" />
           </a>
         </div>
         <div className="project-info">
           <p className="project-paragraph">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos
-            assumenda ratione totam iste suscipit nulla fugit facere
-            exercitationem ex atque harum, repellat voluptas
+            Muay-thai-guy is a product review and blog app all rolled into one.
+            This app combines my long love of muay thai with my new and growing
+            love of programming. All opinions and reviews will be based off my 5
+            years of expierence in Muay Thai.
           </p>
+          <div className="code-links">
+            <h3>
+              Check out the
+              <a target="_blank" href="https://github.com/WAMS24/muay-thai-guy">
+                {" "}
+                code
+              </a>
+            </h3>
+          </div>
         </div>
       </div>
       <div className="projects">
         <div className="project-screenshot">
-          <a className="anchor" href="https://gjam-app.netlify.app/">
-            <img src={GJAM} alt="project2" />
+          <a
+            target="_blank"
+            className="anchor"
+            href="https://gjam-app.netlify.app/"
+          >
+            <img className="screenshot" src={GJAM} alt="project2" />
           </a>
         </div>
         <div className="project-info">
           <p className="project-paragraph">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos
-            assumenda ratione totam iste suscipit nulla fugit facere
-            exercitationem ex atque harum, repellat voluptas
+            This is an eCommerce jam store that lets users log in with the
+            option to view, create, and edit jams. Jams are stored in a database
+            so that users can see jams they created and jams created by other
+            users. If a user is not signed in, they will not be able to edit or
+            create a jam. A shopping cart is available to logged in users so
+            they can add and remove jams.
           </p>
+          <div className="code-links">
+            <h3>
+              Check out the
+              <a target="_blank" href="https://github.com/WAMS24/gjam-app">
+                {" "}
+                code
+              </a>
+            </h3>
+          </div>
         </div>
       </div>
       <div className="projects">
         <div className="project-screenshot">
-          <img src="" alt="project1" />
+          <a
+            target="_blank"
+            className="anchor"
+            href="https://garrettfitnessapp.netlify.app/"
+          >
+            <img className="screenshot" src={FITNESS} alt="project2" />
+          </a>
         </div>
         <div className="project-info">
           <p className="project-paragraph">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos
-            assumenda ratione totam iste suscipit nulla fugit facere
-            exercitationem ex atque harum, repellat voluptas
+            The fitness-app is a simple tool to help users with staying
+            motivated with their fitness routines. One of the worst things a
+            person can do when going to the gym is going without a plan. The
+            fitness-app is a mobile friendly, easy to use way to plan out your
+            workout for the day or week. A history section will be available to
+            show your progress plus upcoming workouts. The history section will
+            also be good for when users are seeking inspiration and wish to
+            switch up their routines a bit.
           </p>
+          <div className="code-links">
+            <h3>
+              Check out the
+              <a target="_blank" href="https://github.com/WAMS24/fitness-app">
+                {" "}
+                code
+              </a>
+            </h3>
+          </div>
         </div>
       </div>
     </div>
